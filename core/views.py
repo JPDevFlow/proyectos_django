21ed234a00835b9ee4f0a938cb89c0484de72da9
JEHYP1021 <jehypo1021@gmail.com>
2025-05-21 11:49:18 +0200

    Modificiacion uso de plantillas y el render

--- core/views.py
+++ core/views.py
@@ -1,20 +1,12 @@
 from django.shortcuts import render, HttpResponse
 
-html_base ='''
-<h1>Mi web personal</h1>
-<ul>
-    <li><a href="/">Potrada</a></li>
-    <li><a href="/about">Acerca de</a></li>
-</ul>
-'''
 # Create your views here.
 
 def home(request):
-    return HttpResponse(html_base+"""
-                        <h2>Portada</h2>
-                        <p>Esto es la portada</p>""")
-
+    return render(request, "core/home.html")
 def about(request):
-    return HttpResponse(html_base+"""
-                        <h2>Acerca de</h2>
-                        <p>Me llamo JPDevFlow soy programdor y desarrolador web</p>""")
+    return render(request, "core/about.html")
+
+def contacto(request):
+    return render(request, "core/contacto.html")
+                        
