18fb0df75d3827835a4b70aa6c0255e9f1a70417
JEHYP1021 <jehypo1021@gmail.com>
2025-05-27 11:09:37 +0200

    Se añade dinamismo a la pagina porfolio

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Project #El punto hace referencia a que el modelo Project se encuentra en el mismo directorio que este archivo views.py
 
 # Create your views here.
 def porfolio(request):
-    return render(request, "porfolio/porfolio.html")                        
+    projects = Project.objects.all()# Obtiene todos los proyectos de la base de datos
+    return render(request, "porfolio/porfolio.html", {"projects": projects})# Renderiza la plantilla porfolio.html y le pasa los proyectos como contexto             
